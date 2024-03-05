a08ce968ccd2058f83fe3e9d41d0be95fba40d03
JiunnTarn <aencil@yeah.net>
2024-03-06 00:19:19 +0800

    chore: updated clash subscription template

--- utils/node_name.py
+++ utils/node_name.py
@@ -42,19 +42,19 @@ class NodeNameGenerator:
         cnt = 0  # Used to avoid infinite loop
         while True:
             if name_type == 'color':
-                name = f"{country}-CF-{self.fake.color_name()}"
+                name = f"{country}-{self.fake.color_name()}"
                 cnt += 1
                 if cnt > 100:
                     name_type = 'alternate'  # If color name is not available, use alternate name
                     cnt = 0  # Reset counter
             elif name_type == 'alternate':
-                name = f"{country}-CF-{self.fake.city()}"  # When color name is not available
+                name = f"{country}-{self.fake.city()}"  # When color name is not available
                 cnt += 1
                 if cnt > 100:
                     name_type = 'random'  # If alternate name is not available, use random name
                     cnt = 0
             else:  # When name_type is invalid
-                name = f"{country}-CF-{uuid.uuid4()}"  # Use UUID to ensure uniqueness
+                name = f"{country}-{uuid.uuid4()}"  # Use UUID to ensure uniqueness
 
             if self.add_emoji:
                 name = f"{country_emoji} {name}"
@@ -74,7 +74,7 @@ class NodeNameGenerator:
             # Try to generate a unique name, first try color name, then try alternate name
             return self.generate_unique_name(country_emoji, country)
         else:
-            name = f"{country}-CF-WARP-{self.counter}"
+            name = f"{country}-{self.counter}"
             if self.add_emoji:
                 name = f"{country_emoji} {name}"
             self.counter += 1
